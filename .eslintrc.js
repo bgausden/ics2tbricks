770bb8878119bfa1859bc4b10681ae95165cc809
Barry Gausden <barryga@Barrys-MBP.ad.orcsoftware.com>
2019-12-19 17:50:03 +0800

    Add ESLint to project

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+    root: true,
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        tsconfigRootDir: __dirname,
+        project: ["./tsconfig.json"],
+        ecmaVersion: 2020,
+    },
+    plugins: ["@typescript-eslint"],
+    extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/eslint-recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+        "standard-with-typescript",
+        "prettier/@typescript-eslint",
+        // "plugin:prettier/recommended",
+        // "plugin:node/recommended",
+    ],
+    rules: {
+        "node/exports-style": ["error", "module.exports"],
+        "node/file-extension-in-import": ["error", "always"],
+        "node/prefer-global/buffer": ["error", "always"],
+        "node/prefer-global/console": ["error", "always"],
+        "node/prefer-global/process": ["error", "always"],
+        "node/prefer-global/url-search-params": ["error", "always"],
+        "node/prefer-global/url": ["error", "always"],
+        "node/prefer-promises/dns": "error",
+        "node/prefer-promises/fs": "error",
+    },
+};
