83b2c1cd553bf5cf5c531cbc0f971680070c2fa9
Barry Gausden <barryga@barrys-mbp.ad.orcsoftware.com>
2020-01-03 18:29:27 +0800

    Initial initialization script for index.html

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,13 @@
 /* eslint-disable space-before-function-paren */
 /* eslint-disable comma-dangle */
 /* eslint-disable @typescript-eslint/interface-name-prefix */
-import { calResourceFromURL, DEFAULT_CALENDAR_URL, DEFAULT_COUNTRY_CODE } from "./ics2tbricks.js"
+import { calResourceFromURL, DEFAULT_CALENDAR_URL, DEFAULT_COUNTRY_CODE } from "./ics2tbricks"
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export function onBodyLoad(): void {
-    const formEL = document.getElementById("ics2calResource")
+export function init(formEL: HTMLElement, url: HTMLElement, countryCode: HTMLElement): void {
+    /* const formEL = document.getElementById("ics2calResource")
     const url = document.getElementById("icsURL")
-    const countryCode = document.getElementById("countryCode")
+    const countryCode = document.getElementById("countryCode") */
     if (formEL instanceof HTMLElement) {
         formEL.addEventListener("submit", function(evt) {
             evt.preventDefault()
@@ -23,3 +23,6 @@ function retrieveAndProcess(url: string, countryCode: string): void {
     // eslint-disable-next-line @typescript-eslint/no-floating-promises
     calResourceFromURL(url, countryCode).finally(() => console.log(`Done`))
 }
+
+console.log(`index.ts loaded.`)
+// onBodyLoad()
